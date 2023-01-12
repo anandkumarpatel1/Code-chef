c31d91576856f65a84128d28d7b20e53b080e977
anand kumar <harshrajsinha1@gmail.com>
2023-01-12 18:54:06 +0530

    code chef

---
+++ Greater_Average.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        float x = (float)(a + b) / 2;
+        if (x > c)
+        {
+            printf("YES \n");
+        }
+        else
+        {
+            printf("NO /n");
+        }
+    }
+    return 0;
+}
